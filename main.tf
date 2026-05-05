3e916aed0f4db043d963458d9511e891341e8978
Naresh Gantala <nareshgantala@gmail.com>
2026-05-05 17:13:39 +0530

    associate public ip

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ resource "azurerm_network_interface" "workstation" {
     name                          = "workstation"
     subnet_id                     = "/subscriptions/9be9bd1a-817e-486f-9b33-1b1f79ed3727/resourceGroups/denmark-east/providers/Microsoft.Network/virtualNetworks/test-virtual-network/subnets/default"
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.workstation.id
   }
 }
 
