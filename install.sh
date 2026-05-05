a414c0d2cb832336c902df4de3e5d51168448904
Naresh Gantala <nareshgantala@gmail.com>
2026-05-05 17:42:17 +0530

    update

--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@
 
 echo Install terraform
 sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo -y
 sudo yum -y install terraform
 
-echo insatall azure cli
+echo download azure cli rpm
 sudo rpm --import https://packages.microsoft.com/keys/microsoft-2025.asc
+echo insatall azure cli rpm package
 sudo dnf install -y https://packages.microsoft.com/config/rhel/10/packages-microsoft-prod.rpm
-sudo dnf install azure-cli
+echo install azure cli
+sudo dnf install azure-cli -y
