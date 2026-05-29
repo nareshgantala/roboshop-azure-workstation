5f7604567701db84c6f021b6a6ef2143e0535de0
Naresh Gantala <nareshgantala@gmail.com>
2026-05-29 16:46:57 +0530

    update install command

--- install.sh
+++ install.sh
@@ -37,4 +37,20 @@ sudo install -o root -g root -m 0755 kubectl /usr/bin/kubectl
 # Clean up local file
 rm -f kubectl
 
+echo "install docker"
+sudo dnf remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine \
+                  podman \
+                  runc
+
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo systemctl enable --now docker
 echo "All installations completed successfully!"
