ffbf0ed0e214cb8fcee14ea9c27226bf7dd9bb2f
Naresh Gantala <nareshgantala@gmail.com>
2026-05-29 16:10:52 +0530

    commented jenkins and added kubectl, kind setup

--- main.tf
+++ main.tf
@@ -82,51 +82,51 @@ resource "null_resource" "ws_name" {
    }
 }
 
-resource "azurerm_linux_virtual_machine" "jenkins" {
-  name                = "jenkins"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  size                =  "Standard_B2s"
-  network_interface_ids = [
-    azurerm_network_interface.main["jenkins"].id,
-  ]
+# resource "azurerm_linux_virtual_machine" "jenkins" {
+#   name                = "jenkins"
+#   resource_group_name = var.resource_group_name
+#   location            = var.location
+#   size                =  "Standard_B2s"
+#   network_interface_ids = [
+#     azurerm_network_interface.main["jenkins"].id,
+#   ]
 
-  admin_username = "devops"
-  admin_password = "Devops@12345"
-  disable_password_authentication = false
-  vtpm_enabled = true
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-  source_image_id = "/subscriptions/9be9bd1a-817e-486f-9b33-1b1f79ed3727/resourceGroups/denmark-east/providers/Microsoft.Compute/galleries/roboshopGallery/images/roboshopImage"
-  provisioner "file" {
-    source      = "./jenkins.sh"
-    destination = "/home/devops/jenkins.sh"
+#   admin_username = "devops"
+#   admin_password = "Devops@12345"
+#   disable_password_authentication = false
+#   vtpm_enabled = true
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
+#   source_image_id = "/subscriptions/9be9bd1a-817e-486f-9b33-1b1f79ed3727/resourceGroups/denmark-east/providers/Microsoft.Compute/galleries/roboshopGallery/images/roboshopImage"
+#   provisioner "file" {
+#     source      = "./jenkins.sh"
+#     destination = "/home/devops/jenkins.sh"
 
-    connection {
-        type     = "ssh"
-        user     = "devops"
-        password = "Devops@12345"
-        host     = azurerm_linux_virtual_machine.jenkins.public_ip_address
-    }
-    }
-}
+#     connection {
+#         type     = "ssh"
+#         user     = "devops"
+#         password = "Devops@12345"
+#         host     = azurerm_linux_virtual_machine.jenkins.public_ip_address
+#     }
+#     }
+# }
 
-resource "null_resource" "jenkins_name" {
-    depends_on = [ azurerm_linux_virtual_machine.jenkins ]
+# resource "null_resource" "jenkins_name" {
+#     depends_on = [ azurerm_linux_virtual_machine.jenkins ]
 
-    provisioner "remote-exec" {
-          connection {
-        type     = "ssh"
-        user     = "devops"
-        password = "Devops@12345"
-        host     = azurerm_linux_virtual_machine.jenkins.public_ip_address
-    }
-    inline = [
-      "chmod +x /home/devops/jenkins.sh",
-      "bash /home/devops/jenkins.sh",
-    ]
+#     provisioner "remote-exec" {
+#           connection {
+#         type     = "ssh"
+#         user     = "devops"
+#         password = "Devops@12345"
+#         host     = azurerm_linux_virtual_machine.jenkins.public_ip_address
+#     }
+#     inline = [
+#       "chmod +x /home/devops/jenkins.sh",
+#       "bash /home/devops/jenkins.sh",
+#     ]
 
-   }
-}
+#    }
+# }
